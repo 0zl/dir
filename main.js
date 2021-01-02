0d1c1c2f72c2335b395dc2b304a5c08b46b6c1ab
0zl <zwiefk0@gmail.com>
2021-01-02 15:39:57 +0700

    💢💢

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 'use strict'
 
+const fs = require('fs')
 const Express = require('express')
 
 Express()
   .set('view engine', 'pug')
   .use(require('helmet')())
+  .use(Express.static('../public-dir'))
   
   .get('/', async (req, res) => {
-    res.end('Zuki Directory')
+    let data = await fs.readdirSync('../public-dir')
+    
+    res.render('index', {
+      data
+    })
   })
   
   .listen(8910, () => console.log('[ # ] Port 8910'))
