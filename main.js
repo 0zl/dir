2b3ff2195e7b17fdcf16a25687d88655511a609e
0zl <zwiefk0@gmail.com>
2021-01-02 15:16:53 +0700

    💢

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const Express = require('express')
+
+Express()
+  .set('view engine', 'pug')
+  .use(require('helmet')())
+  
+  .get('/', async (req, res) => {
+    res.end('Zuki Directory')
+  })
+  
+  .listen(8910, () => console.log('[ # ] Port 8910'))
